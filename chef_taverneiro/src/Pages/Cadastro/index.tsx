2947cd3f043df3e10c282e8ce33d29c99fbf4f45
Larissa-Rahal <rahal.larissa@gmail.com>
2024-06-20 18:44:47 -0300

    Style de cadastro feito ( a testar em outras maquinas)

--- chef_taverneiro/src/Pages/Cadastro/index.tsx
+++ chef_taverneiro/src/Pages/Cadastro/index.tsx
@@ -1,6 +1,13 @@
 import React from "react";
-import { View, Text, TextInput, TouchableOpacity, Image, ImageBackground } from "react-native";
-import { useNavigation } from '@react-navigation/native';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  ImageBackground,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
 import { styles } from "./styleCadastro";
 import background from "../../assets/images/Pagina de Cadastro.png";
 import cadastro from "../../assets/images/balao_cadastrar.png";
@@ -11,7 +18,6 @@ const CadastroScreen = () => {
   return (
     <ImageBackground style={styles.loginBackground} source={background}>
       <View style={styles.loginContainer}>
-
         <View style={styles.loginInputContainer}>
           <TextInput
             placeholder="Login"
@@ -29,17 +35,19 @@ const CadastroScreen = () => {
             secureTextEntry
             style={styles.loginInput}
           />
-          <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+          <TouchableOpacity onPress={() => navigation.navigate("Login")}>
             <Text style={styles.loginCadastroText}>Já tenho cadastro</Text>
           </TouchableOpacity>
         </View>
 
         <View style={styles.loginImageContainer}>
-          <TouchableOpacity style={styles.loginButton} onPress={() => navigation.navigate('Login')}>
+          <TouchableOpacity
+            style={styles.loginButton}
+            onPress={() => navigation.navigate("Login")}
+          >
             <Image source={cadastro} style={styles.loginButtonImage} />
           </TouchableOpacity>
         </View>
-
       </View>
     </ImageBackground>
   );
