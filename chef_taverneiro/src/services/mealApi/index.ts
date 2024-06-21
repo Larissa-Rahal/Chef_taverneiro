ca2f49f49f1233f888aa891c6bf02ccacc9f539a
Arthur <arthur.c.fred@hotmail.com>
2024-06-20 23:47:30 -0300

    modulo picker instalado e importado Search --> filtro de busca por nome, categoria, ingrediente e area(somente em ingles); mealApi --> funções de busca pela api (nome, categoria, ingrediente e area)

--- chef_taverneiro/src/services/mealApi/index.ts
+++ chef_taverneiro/src/services/mealApi/index.ts
@@ -19,3 +19,47 @@ export function getItensByCategory( nome: string ): Promise<AxiosResponse<getMea
     
     return mealApi.get(url);
 }
+
+export interface MealByIngredientProps {
+    strMeal: string,
+    strMealThumb: string,
+    idMeal: number
+}
+
+export interface getMealByIngredientListProps {
+    meals: MealByIngredientProps[]
+}
+
+export function getItensByIngredient(ingredient: string): Promise<AxiosResponse<getMealByIngredientListProps, any>> {
+    const url = 'filter.php?i=' + ingredient;
+    return mealApi.get(url);
+}
+
+export interface MealByNameProps {
+    strMeal: string,
+    strMealThumb: string,
+    idMeal: number
+}
+
+export interface getMealByNameListProps {
+    meals: MealByNameProps[]
+}
+
+export function getItensByName(name: string): Promise<AxiosResponse<getMealByNameListProps, any>> {
+    const url = 'search.php?s=' + name;
+    return mealApi.get(url);
+}
+
+export interface MealByAreaProps {
+    strMeal: string,
+    idMeal: number
+}
+
+export interface getMealByAreaListProps {
+    meals: MealByAreaProps[]
+}
+
+export function getItensByArea(area: string): Promise<AxiosResponse<getMealByAreaListProps, any>> {
+    const url = 'filter.php?a=' + area;
+    return mealApi.get(url);
+}
