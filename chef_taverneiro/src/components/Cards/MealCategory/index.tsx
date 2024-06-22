6b72f63e5771f49901fba1019a287677f3fe1b47
MiguelMFR <guelferreirar@gmail.com>
2024-06-22 13:27:28 -0300

    card de protutos

--- chef_taverneiro/src/components/Cards/MealCategory/index.tsx
+++ chef_taverneiro/src/components/Cards/MealCategory/index.tsx
@@ -1,16 +1,25 @@
-import { TouchableOpacity, Text, Image } from "react-native"
+import { TouchableOpacity, Text, Image, View, Linking } from "react-native"
 import { styles } from "./styles"
 import { MealByCategoryProps } from "../../../@types/interface";
+import { Link, useNavigation } from "@react-navigation/native";
 
 interface MealCategoryComponentProps {
-    item: MealByCategoryProps;
+    item: MealByCategoryProps; 
 }
 
 export const MealCategory = ({ item }: MealCategoryComponentProps) => {
-    return <TouchableOpacity style={styles.buttonMeal} key={item.idMeal}>
-        <Image source={{ uri: item.strMealThumb }} style={styles.cardImage} />
-        <Text style={styles.mealText}>
-            {item.strMeal}
-        </Text>
+
+    if (!item.strMealThumb) {
+        return null; // Não renderiza nada se não houver imagem
+    }
+
+    return <View key={item.idMeal} style={styles.cardContainer}>
+    <Image source={{ uri: item.strMealThumb }} style={styles.cardImage} />
+    <Text style={styles.mealText}>
+        {item.strMeal}
+    </Text>
+    <TouchableOpacity onPress={() => console.log('Link clicado')}>
+      <Text onPress={() => alert('eitaa nao sei como fazer')} style={styles.link}>Learn more</Text>
     </TouchableOpacity>
+</View>
 }
