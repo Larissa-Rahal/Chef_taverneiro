34e549ebb9c2d06bf3889cbb56819ad639b239ea
Larissa-Rahal <rahal.larissa@gmail.com>
2024-06-22 18:23:53 -0300

    Ajuste da profile

--- chef_taverneiro/src/Pages/Profile/index.tsx
+++ chef_taverneiro/src/Pages/Profile/index.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { BottomTabNavigationProp } from "@react-navigation/bottom-tabs/";
 import { RootTabParamList } from "../../Routes/BottomTabRoutes";
 import {
@@ -12,14 +11,6 @@ import {
 import { useEffect, useState } from "react";
 import { styles } from "./styleProfile";
 import background from "../../assets/images/Madeira.png";
-=======
-import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs/';
-import { RootTabParamList } from '../../Routes/BottomTabRoutes';
-import { ImageBackground, Text, View, Image, TextInput, ScrollView } from 'react-native';
-import { useEffect, useState } from 'react';
-import { styles } from "./styleProfile";
-import background from "../../assets/images/Madeira.png"
->>>>>>> 2f2370ff3544bd224ac716859be62fb618ad105d
 import circulo from "../../assets/images/CirculoBranco.png";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faAddressCard } from "@fortawesome/free-solid-svg-icons";
