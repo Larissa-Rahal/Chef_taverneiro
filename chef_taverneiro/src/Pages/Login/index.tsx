8171ba5f2264490805fcbbf5687e32add08d0951
Larissa-Rahal <rahal.larissa@gmail.com>
2024-06-21 22:12:20 -0300

    ajuste do background do Login e do Cadastro

--- chef_taverneiro/src/Pages/Login/index.tsx
+++ chef_taverneiro/src/Pages/Login/index.tsx
@@ -60,7 +60,10 @@ const LoginScreen = () => {
   };
 
   return (
-    <ImageBackground style={styles.background} source={background}>
+    <>
+      <View>
+        <ImageBackground style={styles.background} source={background} />
+      </View>
       <View style={styles.container}>
         <TextInput
           placeholder="Login"
@@ -85,7 +88,7 @@ const LoginScreen = () => {
           <Image source={acessar} />
         </TouchableOpacity>
       </View>
-    </ImageBackground>
+    </>
   );
 };
 
