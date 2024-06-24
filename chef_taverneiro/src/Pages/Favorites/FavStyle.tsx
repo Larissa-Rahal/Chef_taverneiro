673b9c2206c89519c65980dea2fd56f5d6ff5fe7
Larissa-Rahal <rahal.larissa@gmail.com>
2024-06-24 19:10:33 -0300

    Ajuste de styles

--- chef_taverneiro/src/Pages/Favorites/FavStyle.tsx
+++ chef_taverneiro/src/Pages/Favorites/FavStyle.tsx
@@ -2,8 +2,10 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
     favBackground: {
-        flex: 1,
-        resizeMode: 'cover',
-        justifyContent: 'center',
+      flex: 1,
+      resizeMode: 'cover',
+      justifyContent: 'center',
+      minHeight:'100%',
+      minWidth: '100%'
       },
 });
