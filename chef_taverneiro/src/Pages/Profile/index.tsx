20434b70f265698bbb4315e9b4232115e585aa36
Larissa-Rahal <rahal.larissa@gmail.com>
2024-06-25 18:53:50 -0300

    IMAGEM DE BACKGROUND

--- chef_taverneiro/src/Pages/Profile/index.tsx
+++ chef_taverneiro/src/Pages/Profile/index.tsx
@@ -1,7 +1,7 @@
 import { BottomTabNavigationProp } from "@react-navigation/bottom-tabs/";
 import { RootTabParamList } from "../../Routes/BottomTabRoutes";
 import { styles } from "./styleProfile";
-import background from "../../assets/images/Madeira.png";
+import background from "../../assets/images/Perfil Background.png";
 import { ImageBackground, Text, View, Image } from "react-native";
 import circulo from "../../assets/images/CirculoBranco.png";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
